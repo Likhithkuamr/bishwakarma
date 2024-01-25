b4d3cf5a835a41c62ed63783a77e28a6d1aff19a
jokeNquote <139613375+webbag1@users.noreply.github.com>
2024-01-25 17:18:10 +0530

    changes done for mobile

---
+++ toggle.js
@@ -0,0 +1,6 @@
+      const toggleBtnIcon = document.querySelector(".fa-bars");
+      const dropDownMenu = document.querySelector(".dmenu");
+
+      toggleBtnIcon.onclick = function () {
+        dropDownMenu.classList.toggle('open')
+      }
